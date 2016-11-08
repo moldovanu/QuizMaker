63090fc67999b0dffcc60813c8f8243873f24dcf
Dmitrii Fotesco <dmfote@microsoft.com>
2016-11-08 05:47:21 -0800

    Form

--- src/Test/wwwroot/lib/app.js
+++ src/Test/wwwroot/lib/app.js
@@ -1,4 +1,36 @@
-var inputField = document.getElementById("name");
-var myName = inputField.value;
-console.log(myName);
+//function addAnswer(divName) {
+//    console.log("Inside the addAnswer function!");
+//    console.log("passing - " + divName);
+//    let newDiv = document.createElement('div');
+//    newDiv.innerHTML = <label><input type="checkbox" > True < /label>
+//    console.log("new div - " + newDiv);
+//    console.log("element divName = " + document.getElementById(divName));
+//    document.getElementById(divName).appendChild(newDiv);
+//}
+var Question = (function () {
+    function Question(question) {
+        this.arrayOfAnswers = [];
+        this.questionDescr = question;
+    }
+    return Question;
+}());
+var Answer = (function () {
+    function Answer(option, isTrue) {
+        this.option = option;
+        this.isTrue = isTrue;
+    }
+    return Answer;
+}());
+var allQuestions = [];
+var answers = [];
+//function createQuestion() {
+console.log("Inside createQuestion");
+console.log(document.getElementById("questionDescr").value);
+var description = document.getElementById("questionDescr").value;
+var question = new Question(description);
+allQuestions.push(question);
+//}
+//let answer = new Answer("option 1", true);
+//answers.push(answer);
+//question.arrayOfAnswers = answers;
 //# sourceMappingURL=app.js.map
--- src/Test/wwwroot/lib/app.ts
+++ src/Test/wwwroot/lib/app.ts
@@ -1,4 +1,74 @@
-﻿var inputField = <HTMLInputElement>document.getElementById("name");
-var myName = inputField.value;
+﻿//function addAnswer(divName) {
 
-console.log(myName); 
+//    console.log("Inside the addAnswer function!");
+//    console.log("passing - " + divName);
+//    let newDiv = document.createElement('div');
+//    newDiv.innerHTML = <label><input type="checkbox" > True < /label>
+
+//    console.log("new div - " + newDiv);
+//    console.log("element divName = " + document.getElementById(divName));
+//    document.getElementById(divName).appendChild(newDiv);
+//}
+
+
+
+class Question {
+
+  public  questionDescr: string;
+  public arrayOfAnswers = [];
+
+    constructor(question:string) {
+        this.questionDescr = question;
+    }
+}
+
+class Answer {
+
+    public option: string;
+    public isTrue: boolean;
+
+    constructor(option: string, isTrue: boolean) {
+        this.option = option;
+        this.isTrue = isTrue;
+    }
+}
+
+
+let allQuestions = [];
+let answers = [];
+
+//function createQuestion() {
+
+    console.log("Inside createQuestion");
+    console.log((<HTMLInputElement>document.getElementById("questionDescr")).value);
+
+    let description = (<HTMLInputElement>document.getElementById("questionDescr")).value;
+
+let question = new Question(description);
+
+    allQuestions.push(question);
+//}
+
+
+
+
+
+//let answer = new Answer("option 1", true);
+
+//answers.push(answer);
+
+//question.arrayOfAnswers = answers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
